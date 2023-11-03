46161c8607e68aa21e15261e582d4d403b1bab71
Han <mayo3610@naver.com>
2023-11-03 15:31:41 +0900

    Add : reset.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import { useState } from "react";
 
-import "./App.css";
 import { Link, NavLink } from "../node_modules/react-router-dom/dist/index";
-import "./index.css";
 function App() {
   return (
     <>
-      <div>Router Test</div>
-      <h1 class="text-3xl font-bold underline flex">
-        Hello world!
-        <Link to="/video">video Page</Link>
-      </h1>
+      <header>
+        <img src="src/img/header-logo.png" />
+        <button>공유공간 찾기</button>
+        <button>로그인</button>
+      </header>
     </>
   );
 }
