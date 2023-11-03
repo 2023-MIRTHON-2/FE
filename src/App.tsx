8688e51a0ced95b375ddf861cbba8dfffd201a6b
Han <mayo3610@naver.com>
2023-11-03 15:03:35 +0900

    Add : tailwind 설치

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { useState } from "react";
 
 import "./App.css";
 import { Link, NavLink } from "../node_modules/react-router-dom/dist/index";
-
+import "./index.css";
 function App() {
   return (
     <>
       <div>Router Test</div>
-      <Link to="/video">video Page</Link>
+      <h1 class="text-3xl font-bold underline flex">
+        Hello world!
+        <Link to="/video">video Page</Link>
+      </h1>
     </>
   );
 }
