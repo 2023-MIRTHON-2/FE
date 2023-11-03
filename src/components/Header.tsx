8e954b47ec0c1d0baf51b88eed417f748d680e04
Han <mayo3610@naver.com>
2023-11-03 21:42:15 +0900

    Feat : 모달 카드 만들기 (Padding-반응형)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,16 @@
 export default function Header() {
   return (
-    <header class="w-1/1 py-1 px-20 lg:px-60 flex justify-between items-center shadow-md">
+    <header className="w-1/1 py-1 px-20 lg:px-60 flex justify-between items-center shadow-md">
       <div>
-        <img src="src/img/header-logo.png" alt="바로여기 로고" />
+        <img
+          className="image-quality-improve"
+          src="src/img/header-logo.png"
+          alt="바로여기 로고"
+        />
       </div>
       <div>
-        <button class="mr-10">공유공간 찾기</button>
-        <button class="">로그인</button>
+        <button className="mr-10">공유공간 찾기</button>
+        <button className="">로그인</button>
       </div>
     </header>
   );
