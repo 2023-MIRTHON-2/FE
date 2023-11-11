f4d3fd20d0394a93529fc1fb57a26da224086ff3
Han <mayo3610@naver.com>
2023-11-12 03:27:24 +0900

    Refactor : 메인 UI 리팩토링

--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -47,7 +47,9 @@ export default function MainCard({
         </header>
         <main className="mt-6">
           <h1 className="text-xl font-bold">{title}</h1>
-          <p className="text-sm mt-3 font-light">{subTitle}</p>
+          <p className="text-sm mt-3 font-light line-clamp-2">
+            {subTitle.trim()}
+          </p>
         </main>
         <aside className="mt-8 text-xl flex justify-end">
           <span className="mr-1">월</span>
